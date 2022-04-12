8dbdac9fd841d4e2fb1051c4d8791ade5161e0d2
Simon Dutka <SimonDutkaWork@gmail.com>
2022-04-12 23:46:34 +0200

    feat: added read function

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,11 +1,24 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
+
 import './note.scss';
 
 const Note = ({ title, text, id, deleteItem }) => {
+    // State for opening a note
+    const [readNote, setReadNote] = useState(false);
+
+    const toggleNote = () => {
+        if (text.length > 100) {
+            setReadNote((openNote) => !openNote);
+        }
+    };
+
     return (
         <div className='note'>
             <p className='note__title'>{title}</p>
-            <p className='note__text'>{text}</p>
+            <p className={`note__text ${readNote ? 'note__text--open' : ''}`}>
+                {text}
+            </p>
             <div className='note__btn-container'>
                 <button
                     className='note__btn note__btn--delete'
@@ -15,7 +28,11 @@ const Note = ({ title, text, id, deleteItem }) => {
                     Delete
                 </button>
 
-                <button className='note__btn' type='submit'>
+                <button
+                    className='note__btn'
+                    type='submit'
+                    onClick={toggleNote}
+                >
                     Read
                 </button>
             </div>
